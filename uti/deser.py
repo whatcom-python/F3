a2c93b317e0ca20cf8fd7ce82e9700d61f26e3bb
Hayes Williams <dubious.pie@gmail.com>
2011-03-08 00:27:36 -0800

    Switched to fixtures. The data will be automatically added when manage.py syncdb is called.
    Signed-off-by: Peter Rust <peter@cornerstonenw.com>

--- uti/deser.py
+++
@@ -1,8 +0,0 @@
-from django.core import serializers
-
-JSONSerializer = serializers.get_serializer("json")
-json_serializer = JSONSerializer()
-
-# hacks abound
-for obj in serializers.deserialize("json", open('uti/f3_final.json', 'r')):
-    obj.save()
