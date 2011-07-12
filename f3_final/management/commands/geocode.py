6e524f624919f3bfab07a3eb556a93758fbabaa2
Hayes Williams <dubious.pie@gmail.com>
2011-07-11 21:53:30 -0700

    Make geocode command raise correct exception if Something Bad Happens™.

--- f3_final/management/commands/geocode.py
+++ f3_final/management/commands/geocode.py
@@ -17,7 +17,7 @@ class Command( BaseCommand ):
                     try:
                         place, ( lat, lng ) = g.geocode( farm.address + ', WA' )
                     except:
-                        pass
+                        raise CommandError, "There was a error getting an lat/long pair for farm" % ( farm.name )
                     else:
                         print "I think farm \"%s\" is at latitude \"%s\", longitude \"%s\"" % (farm.name, lat, lng)
                         farm.address, farm.latitude, farm.longitude = place, lat, lng
